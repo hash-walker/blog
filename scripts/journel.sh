1771e8f9fed6e543b4ef50f962f780a377907874
hash-walker <hamzafaraz1821@gmail.com>
2025-12-07 15:57:05 +0500

    updated the auto script

--- scripts/journel.sh
+++ scripts/journel.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-filename=/home/hash-walker/github.com/hash-walker/bloggr/content/en/journal/logs/journel_$( date '+%Y-%m-%d' ).md
+filename=/home/hash-walker/github.com/hash-walker/bloggr/content/en/journal/logs/journal_$( date '+%Y-%m-%d' ).md
 
 cat <<EOF> "$filename"
+---
+title: "journal 2025-12-06"
+date: $( date '+%Y-%m-%dT%H:%M:%S%z' )
+draft: true
+---
+
+
 $( date '+%A, %B %d, %Y' ) 
-	
+
 # What did I work on today?
 # What did I learn today?
 # Any blockers?
